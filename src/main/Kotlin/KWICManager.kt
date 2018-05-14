f926b10e5459721803c51c07269643ddeb6f4d30
Nicholas <nicholasmarques@cjr.org.br>
2018-05-14 19:10:37 -0300

    adicionado mkKWIC retornando lista com KWIC feitas sem stop_words

--- src/main/Kotlin/KWICManager.kt
+++ src/main/Kotlin/KWICManager.kt
@@ -1,14 +1,17 @@
 class KWICManager {
 
     fun mkKWIC(frases: List<String>): List<String>{
+        var return_frases: MutableList<String> = mutableListOf()
         var i: Int
         var j: Int
         var frases_line: List<String>
         for(i in 0..frases.size){
             frases_line = frases[i].split(" ")
             for(j in 0..frases_line.size){
-                Shifter.shift(frases_line,j,j)
+                return_frases.add(Shifter.shift(frases_line,j,j).joinToString(" "))
             }
         }
+
+        return return_frases
     }
 }
