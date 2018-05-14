2471cf26f7b6079123c162527d0628d751f22d35
Nicholas <nicholasmarques@cjr.org.br>
2018-05-13 21:20:28 -0300

    Modulo de ler file pronto

--- src/main/Kotlin/FileStorageManager.kt
+++ src/main/Kotlin/FileStorageManager.kt
@@ -1,15 +1,19 @@
+import java.io.File
+
 class FileStorageManager: StorageManager {
     override fun init() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 
     override fun read(file_name: String): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return File(file_name).readLines()
     }
 
     override fun write(file_name: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        File(file_name).printWriter().use { out ->
+            out.write("Eu amo TP2!\nBonifácil é massa!")
+        }
     }
 
     override fun length(): Int {
--- src/test/Kotlin/StepDefs.kt
+++ src/test/Kotlin/StepDefs.kt
@@ -12,9 +12,6 @@ class StepDefs: Pt {
 
         Dado("^que eu tenha um arquivo texto denominado \"([^\"]*)\"$") { arg1: String ->
             storage_manager.write(arg1)
-//            File(arg1).printWriter().use { out ->
-//                out.write("Eu amo TP2!")
-//            }
             //println(arg1)
             //throw PendingException()
         }
@@ -28,6 +25,7 @@ class StepDefs: Pt {
         Entao("^a primera frase lida deve ser \"([^\"]*)\"$") { arg1: String ->
             // Write code here that turns the phrase above into concrete actions
             Assert.assertEquals(arg1,lines[0])
+            Assert.assertEquals("Bonifácil é massa!",lines[1])
             //throw PendingException()
         }
     }
